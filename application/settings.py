049911b518077328abf5e337bdce10d40657c43c
Tom Rees <zephod@gmail.com>
2012-07-27 20:26:05 +0100

    Fix db link

--- application/settings.py
+++ application/settings.py
@@ -6,8 +6,8 @@ TESTING = True
 DB_CON = os.environ.get('DATABASE_URL')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')
 
-if not PROD:
-   DB_CON = 'postgresql://localhost/zephod'
+#if not PROD:
+   #DB_CON = 'postgresql://localhost/zephod'
 
 
 SECRET_KEY = 'aTi1LqrwnK01XnRlVMh4HVYZ'
