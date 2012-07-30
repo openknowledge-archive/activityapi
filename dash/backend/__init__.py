3314f8e2758f9e877dc508ad68900846ed9d2291
Tom Rees <zephod@gmail.com>
2012-07-30 13:35:46 +0100

    Don't hardcode DB_CON

--- dash/backend/__init__.py
+++ dash/backend/__init__.py
@@ -1,7 +1,8 @@
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker, scoped_session
+import settings
 
-engine = create_engine('postgresql://localhost/zephod', echo=True)
+engine = create_engine(settings.DB_CON, echo=True)
 Session = scoped_session(sessionmaker(engine))
 
 import model
