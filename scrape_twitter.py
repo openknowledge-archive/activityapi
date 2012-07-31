95d2612fec5c77a55b05a306aa6fba74925e182f
Tom Rees <zephod@gmail.com>
2012-07-31 18:37:58 +0100

    Scrape_twitter.py can use the library code in dash.scrapers.

--- scrape_twitter.py
+++ scrape_twitter.py
@@ -1,40 +1,13 @@
-from pprint import pprint
-import sqlite3 as lite
-import requests
-import json
+#!/usr/bin/env python 
 
-def connect_db(filename):
-    assert file(filename)
-    return lite.connect( filename )
-
-def scrape_tweets( con, endpoint, username, since_id='' ):
-    payload = { 'q' : 'from:%s'%username }
-    if since_id:
-        payload['since_id'] = since_id
-    r = requests.get( endpoint, params=payload )
-
-    print r.url
-    assert r.status_code==200
-    obj = json.loads( r.content )
-    print 'Got %d tweets from %s' % ( len(obj['results']), username )
-    cur = con.cursor()
-    for tweet in obj['results']:
-        query = "insert into tw values('%s','%s','%s','%s')"
-        query %= (  tweet['from_user'],
-                    tweet['id_str'],
-                    tweet['created_at'],
-                    tweet['text'])
-        print query
-        cur.execute( query )
+import dash.scraper
+import argparse
 
 if __name__=='__main__':
-    import argparse
-
     parser = argparse.ArgumentParser(description='Download tweets from the given user into the database')
-    parser.add_argument('endpoint', type=str, help='Twitter search endpoint'),
+    parser.add_argument('url', type=str, help='Twitter API url endpoint'),
     parser.add_argument('user', type=str, help='Twitter Username'),
     parser.add_argument('--since', default='', type=str, help='Download tweets since this tweet ID'),
-    parser.add_argument('--db', default='database.sqlite', type=str, help='Location of SQLite database'),
+    parser.add_argument('--commit', action='store_true', help='To commit to the database'),
     arg = parser.parse_args()
-    with connect_db(arg.db) as con:
-        scrape_tweets(con, arg.endpoint, arg.user, arg.since)
+    dash.scraper.scrape_tweets(arg.url, arg.user, since_id=arg.since, commit=arg.commit)
