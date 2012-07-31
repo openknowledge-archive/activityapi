796277fa7633e9e1d2752c32209207d0fbc04c53
Tom Rees <zephod@gmail.com>
2012-07-31 18:39:25 +0100

    Remove cruft util code destined for wordpress' MySQL server.

--- dash/util.py
+++ dash/util.py
@@ -2,17 +2,6 @@ import csv
 import re
 import string
 
-def decode_iso(data):
-    return unicode(data, "iso-8859-1")
-
-def encode_utf8(unicode_string):
-    return unicode_string.encode("utf-8")
-
-def read_csv(filename,delimiter='\t'):
-    with open(filename) as f:
-        reader = csv.DictReader(f,delimiter=delimiter)
-        return [row for row in reader]
-
 def clean_user(user):
     """Remove NULL strings and tidy user parameters"""
     for (k,v) in user.items():
