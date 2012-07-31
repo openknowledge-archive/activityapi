327c738bb846a01a18d996925eddc4f8bcdde40e
Tom Rees <zephod@gmail.com>
2012-07-31 22:56:25 +0100

    Don't be a noisy database by default.

--- dash/backend/__init__.py
+++ dash/backend/__init__.py
@@ -3,10 +3,12 @@ from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 database_url = os.environ.get('DATABASE_URL')
+database_echo = os.environ.get('DATABASE_ECHO', '').lower()=='true'
+
 if not database_url:
     raise ValueError('No DATABASE_URL defined in the environment. Try running:\n          $ export DATABASE_URL=postgresql://user:pass@localhost/mydatabase')
 
-engine = create_engine(database_url, echo=True)
+engine = create_engine(database_url,echo=database_echo)
 Session = scoped_session(sessionmaker(engine))
 
 import model
