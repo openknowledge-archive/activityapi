fa7ca30f64e2c02e57e153a915805d6eb36afa62
Tom Rees <zephod@gmail.com>
2012-08-01 00:02:39 +0100

    Turned front-end into API system

--- dash/frontend/api.py
+++ dash/frontend/api.py
@@ -1,4 +1,5 @@
 from dash.backend import Session
+from dash.backend.model import *
 import json
 
 def index():
@@ -7,3 +8,14 @@ def index():
 def twitter():
     out = {'hello':'world'}
     return json.dumps(out)
+
+def timestamps():
+    limit = 10
+    count = Session.query(Timestamp).count()
+    q = Session.query(Timestamp).order_by(Timestamp.id.desc()).limit(limit)
+    data = { 
+        'total': count, 
+        'limit': limit, 
+        'data': [ [x.id,x.now] for x in q ] 
+    }
+    return json.dumps(data)
--- dash/frontend/urls.py
+++ dash/frontend/urls.py
@@ -7,4 +7,6 @@ def homepage():
     return app.send_static_file('index.html')
 
 app.add_url_rule('/api/', 'api', view_func=api.index, methods=('POST', 'GET'))
-app.add_url_rule('/api/twitter', 'api.twitter', view_func=api.twitter, methods=('POST', 'GET'))
+app.add_url_rule('/api/twitter', 'api.twitter', view_func=api.twitter)
+app.add_url_rule('/api/timestamps', 'api.timestamps', view_func=api.timestamps)
+
