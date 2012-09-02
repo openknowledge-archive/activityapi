cbab5b4bf436bea604688f6fbea0ee22369c364a
Tom Rees <zephod@gmail.com>
2012-09-02 23:47:45 +0100

    Refactored scrape_timestamp.py to follow standard pattern.

--- scrape_timestamp.py
+++ scrape_timestamp.py
@@ -4,9 +4,15 @@ from dash.backend import Session
 from dash.backend.model import Timestamp
 from time import gmtime, strftime
 
-timestring = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-t = Timestamp(timestring)
-Session.add(t)
-Session.commit()
+def main():
+    parser = argparse.ArgumentParser(description='Insert the current timestamp into the database. Used to track uptime, debugging etc.')
+    parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', help='Verbose output')
+    arg = parser.parse_args()
+    timestring = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    if arg.verbose: print 'Adding timestamp %s...' % timestring
+    t = Timestamp(timestring)
+    Session.add(t)
+    Session.commit()
 
-
+if __name__=='__main__':
+    main()
