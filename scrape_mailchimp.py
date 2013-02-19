ce59e27e4a7e49ac1731a4cf5c2f4b3170dccc74
Tom Rees <zephod@gmail.com>
2013-02-19 16:49:58 +0000

    Refactor scrape_mailchimp

--- scrape_mailchimp.py
+++ scrape_mailchimp.py
@@ -1,14 +1,50 @@
 #!/usr/bin/env python
 
 import argparse
+import os
+from postmonkey import PostMonkey
+from pprint import pprint
+from datetime import datetime,timedelta
+from dash.backend import Session, model
 
-from dash import mailchimp
+def snapshot_mailchimp(verbose=False):
+    api_key = os.environ.get('MAILCHIMP_API_KEY')
+    assert api_key, 'No MAILCHIMP_API_KEY defined in environment.'
+    pm = PostMonkey(api_key, timeout=10)
+    ping_string = pm.ping()
+    expected = u'Everything\'s Chimpy!'
+    assert ping_string==expected, 'Bad handshake, got "%s", expected "%s"' % (ping_string,expected)
+    if verbose:
+        print 'handshake ok'
+    lists = pm.lists()
+    if not 'data' in lists:
+        print 'Got bad lists object from server.'
+        pprint(lists)
+        raise ValueError('Bad lists object from server')
+    # Snapshot creation code...
+    today = datetime.now().date()
+    for l in lists['data']:
+        try:
+            if verbose: print 'Scraping %s...' % l['name']
+            latest = Session.query(model.SnapshotOfMailchimp)\
+                    .filter(model.SnapshotOfMailchimp.name==l['name'])\
+                    .order_by(model.SnapshotOfMailchimp.timestamp.desc())\
+                    .first()
+            if latest and latest.timestamp>=today:
+                if verbose: print ' -> most recent snapshots have already been processed.'
+                continue
+            snapshot = model.SnapshotOfMailchimp(\
+                    name = l['name'],\
+                    members = l['stats']['member_count'],
+                    timestamp = today)
+            if verbose: print '  -> ',snapshot.toJson()
+            Session.add(snapshot)
+            Session.commit()
+        except Exception, e:
+            pprint({'list':l,'exception':str(e)})
 
-def main():
+if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Scrape Mailchimp for daily stats.')
     parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', help='Verbose output')
     arg = parser.parse_args()
-    mailchimp.snapshot_mailchimp( verbose=arg.verbose )
-
-if __name__=='__main__':
-    main()
+    snapshot_mailchimp( verbose=arg.verbose )
