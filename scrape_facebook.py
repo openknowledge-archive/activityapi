349b2dbf9d2cc99999ff3ac446a137ce1b8b7fd2
Tom Rees <zephod@gmail.com>
2013-02-19 16:52:35 +0000

    Refactor facebook scraper

--- scrape_facebook.py
+++ scrape_facebook.py
@@ -1,14 +1,37 @@
 #!/usr/bin/env python
 
 import argparse
+import os
+import facebook
+from pprint import pprint
+from datetime import datetime,timedelta
+from dash.backend import Session, model
 
-from dash import fb
+def snapshot_facebook(verbose=False):
+    api = facebook.GraphAPI()
+    obj = api.get_object('/OKFNetwork')
+    if not 'likes' in obj:
+        print 'Got bad object from server.'
+        pprint(obj)
+        raise ValueError('Bad object from server')
+    likes = obj['likes']
+    if verbose:
+        print 'Likes today: %d' % likes
+    # Snapshot creation code...
+    today = datetime.now().date()
+    latest = Session.query(model.SnapshotOfFacebook)\
+            .order_by(model.SnapshotOfFacebook.timestamp.desc())\
+            .first()
+    if latest and latest.timestamp>=today:
+        if verbose: print ' -> most recent snapshots have already been processed.'
+        return
+    snapshot = model.SnapshotOfFacebook(likes=likes, timestamp=today)
+    if verbose: print '  -> ',snapshot.toJson()
+    Session.add(snapshot)
+    Session.commit()
 
-def main():
+if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Scrape Facebook for daily stats.')
     parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', help='Verbose output')
     arg = parser.parse_args()
-    fb.snapshot_facebook( verbose=arg.verbose )
-
-if __name__=='__main__':
-    main()
+    snapshot_facebook( verbose=arg.verbose )
